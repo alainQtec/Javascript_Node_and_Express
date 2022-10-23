79dcd4178d1bb16b3338d087caa57c8533c42f0b
Alain Herve <alain.1337dev@outlook.com>
2022-10-23 16:02:27 +0200

    Update utils.js

--- Node/utils.js
+++ Node/utils.js
@@ -1,4 +1,14 @@
-const sayHi = (name) => {
+import os from "os";
+export const currentOs = {
+  Name: os.type(),
+  Release: os.release(),
+  Ram: os.totalmem(),
+  Uptime: os.uptime(),
+};
+export const sayHi = (name) => {
+  if (!name) {
+    name = os.userInfo().username;
+  }
   console.log("Hi", name);
 };
 

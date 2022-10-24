ec95fb2cd04c3a7bb96f401dea7c93d8e04807c0
Alain Herve <alain.1337dev@outlook.com>
2022-10-24 07:37:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,11 +16,26 @@ import variables, { sayHi } from "./Node/Fundamentals.js";
 import { currentOs, sayHi as _sayHi } from "./Node/utils.js";
 import path from "path";
 import { startmyServer } from "./Node/Http.js";
+import { readFile } from "fs/promises";
 
-console.log(variables);
-_sayHi();
-sayHi("Mom");
-console.log(currentOs);
-console.log(path.sep);
-console.log("normalised path :", path.join("content", "subfolder", "test.txt"));
-startmyServer();
+function testImportedStuff() {
+  console.log(variables);
+  _sayHi();
+  sayHi("Mom");
+  console.log(currentOs);
+  console.log(path.sep);
+  console.log(
+    "normalised path :",
+    path.join("content", "subfolder", "test.txt")
+  );
+}
+console.log("starting the first task");
+readFile("./assets/file-one.txt", "utf8", (err, res) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(res);
+  console.log("Completed the first Task");
+});
+console.log("starting the next task");
+// startmyServer();

063a2baef02c5502a9155aaf088b439a5f16bc6e
Alain Herve <alain.1337dev@outlook.com>
2022-10-31 13:07:06 +0200

    Update app.js

--- vanillaJS_spa/app.js
+++ vanillaJS_spa/app.js
@@ -1,25 +1,34 @@
-console.log("app running ...");
-
+const mainPage = document.getElementById("main-page");
 // document.write('<html><body><h2>HTML</h2></body></html>');
 // var tag_id = document.getElementById('tagid');
 // var newNode = document.createElement('p');
 // newNode.appendChild(document.createTextNode('html string'));
-let title = 'My Document';
+const routes = {
+  404: "/views/404.html",
+  "/": "/views/Home.html",
+  "/Dashboard": "/views/Dashboard.html",
+  "/Settings": "/views/Settings.html",
+};
 
-// language=HTML
-if (typeof document == "undefined") {
-    console.log("DOM not found!")
-}else {
-    document.write(`    
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>${title}</title>
-    </head>
-    <body>
-        <p>lorem ipsum .... abcbdbef</p>
-    </body>
-    </html>
-    `);
-}
+const handleLocation = async () => {
+  const path = window.location.pathname;
+  const route = routes[path] || routes[404];
+  const html = await fetch(route).then((data) => data.text());
+  mainPage.innerHTML = html;
+  console.log(html);
+};
+
+window.route = (e) => {
+  const { event } = window;
+  e = e || event;
+  e.preventDefault();
+  let url = e.target.href;
+  window.history.pushState({}, "", url);
+  handleLocation()
+    .then(() => console.log("viewing:", url))
+    .catch((err) => console.log("Error Occurred reason: ", err));
+};
+window.onpopstate = handleLocation;
+handleLocation().then((r) => console.log(r));
+
+console.log("app running ...");

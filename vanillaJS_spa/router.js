7d362bad29c86f56ac89968e3b4b74412d49ba28
Alain Herve <alain.1337dev@outlook.com>
2022-10-31 13:06:43 +0200

    Delete router.js

--- vanillaJS_spa/router.js
+++
@@ -1,9 +0,0 @@
-window.route = (e)=> {
-    const {event} = window;
-    e = e || event;
-    e.preventDefault();
-    let url = e.target.href;
-    window.history.pushState({},"", url);
-    console.log("viewing:", url)
-};
-
